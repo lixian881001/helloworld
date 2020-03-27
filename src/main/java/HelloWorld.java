bf75a3b9dc79679ff38cbd88f1454065abd3f701
xian <lixian881001@126.com>
2020-03-27 16:48:14 +0800

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * @author lixian
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @description ${description}
+ * @date 2020/3/27 16:45
+ * @email lixian881001@126.com
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
